06cb46a0ef02199ccd73c8d79514ad83ecbf39e3
FatimaMahmud <13besefmahmud@seecs.edu.pk>
2016-05-22 19:22:24 +0500

    Scheme File

---
+++ FatimaMahmud_5406.scm
@@ -0,0 +1,25 @@
+#lang scheme
+
+(define NOT  (λ (m) (λ (a b) (m b a))))
+
+(define OR   (λ (m n) (λ (a b)(n a (m a b)))))
+
+(define AND  (λ (m n) (λ (a b)(n (m a b) b))))
+
+(define add  (λ (m n s z) (m s (n s z))))
+
+(define pred (λ (n)( n (λ (g k)( (g 1) (λ (u) ((add(g k)) 1 ) k))) (λ (v)( 0)) 0)))
+
+(define subtract  (λ (m n) (n pred m)))
+
+(define true (λ (x y) (x)))
+
+(define false (λ (x y) (y)))
+
+(define IsZero    (λ (n) ( n (λ (x) (false) true))))
+
+(define LEQ    (λ (m n) ( IsZero (subtract m n))))
+
+(define GEQ    (λ (m n) (NOT (LEQ m n))))
+
+"It works"
